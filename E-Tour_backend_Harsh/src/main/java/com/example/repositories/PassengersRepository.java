6e3e300a9ae93e0893dd71f0ee3221d1cb72c384
Harshal0926 <harshal.hra@gmail.com>
2023-10-26 11:52:18 +0530

    Backend

---
+++ E-Tour_backend_Harsh/src/main/java/com/example/repositories/PassengersRepository.java
@@ -0,0 +1,25 @@
+package com.example.repositories;
+
+
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.example.entities.Passengers;
+@Repository
+@Transactional
+public interface PassengersRepository extends JpaRepository<Passengers, Integer>{
+
+	
+	@Query(value="select * from passengers where customerid = :id",nativeQuery = true)
+	public Optional<Passengers> getinfo(@Param("id") int id);
+     
+}
